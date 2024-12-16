2b4abb2a4a34d668606b321f1d0a85f1aa427a9e
Charlie <charrod796@gmail.com>
2024-12-16 18:58:42 -0500

    footer tweak

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,11 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <div className="py-4 text-center text-gray-500">
+    <div className="py-4 text-center">
       <div className="mt-2">
         <Link 
           href='https://ko-fi.com/W7W317JUAM'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-500 hover:text-blue-700'
+          className='text-blue-500 hover:text-blue-600'
         >
           Buy me a coffee ☕
         </Link>
